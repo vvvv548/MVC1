3909330a0f556508d2d1c5428c92e2d9b49fc927
dazzle <vvvvv548@gmail.com>
2016-03-24 09:41:12 +0800

    首次新增專案

---
+++ MVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC1.Startup))]
+namespace MVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
