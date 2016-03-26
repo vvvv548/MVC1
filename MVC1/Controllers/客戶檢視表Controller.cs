0beca5c25bf9f2a1fd3d2774ce0af3c42f557fe6
dazzle <vvvvv548@gmail.com>
2016-03-27 00:08:21 +0800

    剩餘查詢功能，目前花費時間約3小時

---
+++ MVC1/Controllers/客戶檢視表Controller.cs
@@ -0,0 +1,19 @@
+﻿using MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class 客戶檢視表Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+        // GET: 客戶檢視表
+        public ActionResult Index()
+        {            
+            return View(db.客戶檢視表.ToList());
+        }
+    }
+}
---
+++ MVC1/Models/客戶聯絡人.Partial.cs
@@ -0,0 +1,57 @@
+namespace MVC1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(客戶聯絡人MetaData))]
+    public partial class 客戶聯絡人 : IValidatableObject
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var data = db.Database.SqlQuery<客戶聯絡人>(
+                @"SELECT * FROM dbo.客戶聯絡人 WHERE 客戶Id =@p0 AND
+                  Email=@p1", this.客戶Id, this.Email);
+
+
+            if (data!=null)
+            {
+                yield return new ValidationResult("相同一個客戶下的聯絡人Email不可重複！請重新輸入！",
+                    new string[] { "Email" });
+            }
+        }
+    }
+
+    public partial class 客戶聯絡人MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int 客戶Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 職稱 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 姓名 { get; set; }
+        
+        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [Required]
+        [RegularExpression(@"^[_a-z0-9-]+([.][_a-z0-9-]+)*@[a-z0-9-]+([.][a-z0-9-]+)*$", ErrorMessage ="請輸入符合規範的Email")]
+        public string Email { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [RegularExpression(@"\d{4}-\d{6}", ErrorMessage = "請輸入正確的手機號碼 EX: 09XX-XXXXXX")]
+        public string 手機 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string 電話 { get; set; }
+        public Nullable<bool> 是否已刪除 { get; set; }
+    
+        public virtual 客戶資料 客戶資料 { get; set; }
+    }
+}
